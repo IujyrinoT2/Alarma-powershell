871e6e8c6a45f835407bb5d0df67fe9944933235
Iujy <IujyT2@protonmail.com>
2021-04-09 01:44:15 +0300

    Created the initial script which is able to take the system time and compare it to a hardcoded alarm time by converting it to integer

---
+++ alarma.ps1
@@ -0,0 +1,26 @@
+﻿Write-Output "doamne ajuta`n"
+
+
+$time = get-date -format "HH"
+$time = [int]$time
+$time = $time * 100 + [int](get-date -format "mm")
+Write-Output "$time`n"
+
+$alarmtime = 100
+Write-Output "$alarmtime`n"
+
+while ( $time -lt $alarmtime ) {
+    $time = get-date -format "HH"
+$time = [int]$time
+$time = $time * 100 + [int](get-date -format "mm")
+    Write-Output "inca nu"
+    write-output "$time / $alarmtime"
+    }
+
+    if ( $time -eq $alarmtime ) {
+        Write-Output "YES"
+    }
+    else {
+        Write-Output "CE PULA MEA"
+    }
+
